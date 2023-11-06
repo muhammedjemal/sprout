c23fc04359cad8de50c05d5ef0cca00a127f7955
jems jr <108370284+muhammedjemal@users.noreply.github.com>
2023-11-06 11:40:35 +0300

    user agent changing user agent disable commit

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ await page.evaluate(()=>{
 
 })
 
-await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36');
+// await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36');
 
 // await submitBtn.click();
 
